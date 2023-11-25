ea41594c1c34e30938f1b817990cf9bfb52b751b
cmdspeed <cmdspeed@gmail.com>
2023-11-25 17:19:18 +0100

    add register and login, bcrypt, jwt

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,55 @@
-const { User } = require("../../models/user");
+const { User } = require("../../models");
+const { SECRET_KEY } = process.env;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      status: "error",
+      code: 401,
+      data: {
+        message: "Email or password invalid",
+      },
+    });
+  }
+  const passwordCheck = bcrypt.compareSync(password, user.password);
+
+  if (!passwordCheck) {
+    return res.status(401).json({
+      status: "error",
+      code: 401,
+      data: {
+        message: "Email or password invalid",
+      },
+    });
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
+  await User.findByIdAndUpdate(user.id, { token });
+
+  return res.json({
+    status: "success",
+    code: 200,
+    data: {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        userId: user._id,
+      },
+    },
+  });
+};
 
 module.exports = login;
--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,11 +1,24 @@
 const { User } = require("../../models");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
     const user = await User.findOne({ email });
-    await User.create({ name, email, password });
+    if (user) {
+      return res.status(409).json({
+        status: "error",
+        code: 409,
+        data: {
+          message: "user already exists",
+        },
+      });
+    }
+
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+    await User.create({ name, email, password: hashPassword });
     const newUser = await User.findOne({ email });
     await newUser.save();
     return res.status(201).json({
